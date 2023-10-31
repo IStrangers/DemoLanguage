2712a587b8a401b1cf4a1cc5610a96ebeb54c07d
xjh <xjhit28@163.com>
2023-10-31 09:25:41 +0800

    20231031

--- vm/compiler.go
+++ vm/compiler.go
@@ -1,6 +1,24 @@
 package vm
 
-import "DemoLanguage/ast"
+import (
+	"DemoLanguage/ast"
+	"DemoLanguage/file"
+	"fmt"
+)
+
+type CompilerError struct {
+	Message string
+	File    *file.File
+	Offset  int
+}
+
+type CompilerSyntaxError struct {
+	CompilerError
+}
+
+type CompilerReferenceError struct {
+	CompilerError
+}
 
 type Compiler struct {
 	program *Program
@@ -9,4 +27,22 @@ type Compiler struct {
 func (self *Compiler) compile(in *ast.Program) {
 	body := in.Body
 	self.compileStatements(body, true)
+}
+
+func (self *Compiler) throwSyntaxError(offset int, format string, args ...any) CompiledExpression {
+	panic(&CompilerSyntaxError{
+		CompilerError{
+			File:    self.program.file,
+			Offset:  offset,
+			Message: fmt.Sprintf(format, args),
+		},
+	})
+	return nil
+}
+
+func (self *Compiler) errorAssert(cond bool, offset int, message string, args ...any) CompiledExpression {
+	if cond {
+		return nil
+	}
+	return self.throwSyntaxError(offset, "Compiler error: "+message, args...)
 }
--- vm/compiler_expression.go
+++ vm/compiler_expression.go
@@ -3,6 +3,21 @@ package vm
 import "DemoLanguage/ast"
 
 type CompiledExpression interface {
+	isConstExpr() bool
+}
+
+type CompiledBaseExpression struct {
+	compile *Compiler
+	offset  int
+}
+
+type CompiledLiteralExpression struct {
+	base  CompiledBaseExpression
+	value Value
+}
+
+func (self CompiledLiteralExpression) isConstExpr() bool {
+	return true
 }
 
 func (self *Compiler) compileExpression(expression ast.Expression) CompiledExpression {
@@ -13,17 +28,37 @@ func (self *Compiler) compileExpression(expression ast.Expression) CompiledExpre
 		return self.compileNumberLiteral(expr)
 	case *ast.StringLiteral:
 		return self.compileStringLiteral(expr)
+	default:
+		return self.errorAssert(false, int(expression.StartIndex())-1, "Unknown expression type: %T", expression)
 	}
 }
 
 func (self *Compiler) compileNullLiteral(expr *ast.NullLiteral) CompiledExpression {
-
+	return &CompiledLiteralExpression{
+		CompiledBaseExpression{self, int(expr.StartIndex()) - 1},
+		Const_Null_Value,
+	}
 }
 
 func (self *Compiler) compileNumberLiteral(expr *ast.NumberLiteral) CompiledExpression {
-
+	var value Value
+	switch val := expr.Value.(type) {
+	case int64:
+		value = ToIntValue(val)
+	case float64:
+		value = ToFloatValue(val)
+	default:
+		return self.errorAssert(false, int(expr.StartIndex())-1, "Unsupported number literal type: %T", expr.Value)
+	}
+	return &CompiledLiteralExpression{
+		CompiledBaseExpression{self, int(expr.StartIndex()) - 1},
+		value,
+	}
 }
 
 func (self *Compiler) compileStringLiteral(expr *ast.StringLiteral) CompiledExpression {
-
+	return &CompiledLiteralExpression{
+		CompiledBaseExpression{self, int(expr.StartIndex()) - 1},
+		ToStringValue(expr.Value),
+	}
 }
